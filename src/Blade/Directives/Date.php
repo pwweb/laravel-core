955808545d84c8dc125e929747e0c2fe53863fee
Frank Pillukeit <clients@pw-websolutions.com>
2020-04-23 14:33:10 +0200

    Adds Date blade directive

---
+++ src/Blade/Directives/Date.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace PWWEB\Core\Blade\Directives;
+
+use Illuminate\Support\Str;
+use PWWEB\Core\Blade\Directive;
+
+/**
+ * PWWEB\Core\Blade\Directive Date.
+ *
+ * Blade directive to check whether one or multiple expressions are not null.
+ *
+ * @author    Frank Pillukeit <frank.pillukeit@pw-websolutions.com>
+ * @author    Richard Browne <richard.browne@pw-websolutions.com
+ * @copyright 2020 pw-websolutions.com
+ * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @method    handle()
+ */
+
+class Date extends Directive
+{
+    /**
+     * Constructor for the Date Directive.
+     */
+    public function __construct()
+    {
+        // Set to false, to not generate a @enddate directive.
+        parent::__construct(false);
+    }
+
+    /**
+     * Handle the functionality for the blade directive.
+     *
+     * @param string $expression List of expressions
+     *
+     * @return string
+     */
+    public function handle($expression)
+    {
+        if (true === Str::contains($expression, ',')) {
+            $expression = self::multipleArgs($expression);
+
+            $date   = $expression->get(0);
+            $format = self::stripQuotes($expression->get(1));
+        } else {
+            $date   = $expression;
+            $format = 'd.m.Y H:i:s';
+        }
+
+        return implode(
+            '',
+            [
+                "<?php if (false === is_null({$date})) : ?>",
+                "<?php echo {$date}->format('".$format."') ?>",
+                '<?php endif; ?>',
+            ]
+        );
+    }
+}
