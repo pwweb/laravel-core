77c2b0deeac636b786d659c38fd5546659aa6c98
Frank Pillukeit <clients@pw-websolutions.com>
2020-04-25 19:19:13 +0200

    Fixes style CI

--- src/Requests/CreateUserRequest.php
+++ src/Requests/CreateUserRequest.php
@@ -18,7 +18,6 @@ use PWWEB\Core\Models\User;
  */
 class CreateUserRequest extends FormRequest
 {
-
     /**
      * Determine if the user is authorized to make this request.
      *
--- src/Requests/UpdateUserRequest.php
+++ src/Requests/UpdateUserRequest.php
@@ -18,7 +18,6 @@ use PWWEB\Core\Models\User;
  */
 class UpdateUserRequest extends FormRequest
 {
-
     /**
      * Determine if the user is authorized to make this request.
      *
