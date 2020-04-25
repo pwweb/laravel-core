c4c75e912d80b0d819c61b82de5f67bd9f8e1183
Frank Pillukeit <clients@pw-websolutions.com>
2020-04-25 16:16:26 +0200

    Adjusts comments

--- src/Traits/Migratable.php
+++ src/Traits/Migratable.php
@@ -7,7 +7,8 @@ namespace PWWEB\Core\Traits;
  *
  * Trait providing additional functionalities for migratable models.
  *
- * @author    Frank Pillukeit <clients@pw-websolutions.com>
+ * @author    Frank Pillukeit <frank.pillukeit@pw-websolutions.com>
+ * @author    Richard Browne <richard.browne@pw-websolutions.com
  * @copyright 2020 pw-websolutions.com
  * @license   http://www.opensource.org/licenses/mit-license.html  MIT License
  */
