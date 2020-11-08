c863e1d777325477b41cf18ef966f07cc57eba74
Richard Browne <r.a.browne@gmail.com>
2020-11-08 17:49:07 +0000

    Corrections to currency seeder.

--- src/Database/Seeders/CurrencySeeder.php
+++ src/Database/Seeders/CurrencySeeder.php
@@ -34,19 +34,20 @@ class CurrencySeeder extends Seeder
         $currencies[] = ['name' => 'Brazilian Real', 'iso' => 'BRL', 'numeric_code' => '986', 'active' => '0', 'entity_code' => 'R$', 'standard' => '0'];
         $currencies[] = ['name' => 'Canadian Dollar', 'iso' => 'CAD', 'numeric_code' => '124', 'active' => '0', 'entity_code' => 'C$', 'standard' => '0'];
         $currencies[] = ['name' => 'Swiss Franc', 'iso' => 'CHF', 'numeric_code' => '756', 'active' => '0', 'entity_code' => 'SFr', 'standard' => '0'];
-        $currencies[] = ['name' => 'Yuan Renminbi', 'iso' => 'CNY', 'numeric_code' => '156', 'active' => '0', 'entity_code' => '¥', 'standard' => '0'];
+        $currencies[] = ['name' => 'Chinese Yuan Renminbi', 'iso' => 'CNY', 'numeric_code' => '156', 'active' => '0', 'entity_code' => '¥', 'standard' => '0'];
         $currencies[] = ['name' => 'Czech Koruna', 'iso' => 'CZK', 'numeric_code' => '203', 'active' => '0', 'entity_code' => 'Kč', 'standard' => '0'];
         $currencies[] = ['name' => 'Danish Krone', 'iso' => 'DKK', 'numeric_code' => '208', 'active' => '0', 'entity_code' => 'kr', 'standard' => '0'];
         $currencies[] = ['name' => 'Euro', 'iso' => 'EUR', 'numeric_code' => '978', 'active' => '1', 'entity_code' => '€', 'standard' => '0'];
         $currencies[] = ['name' => 'Pound Sterling', 'iso' => 'GBP', 'numeric_code' => '826', 'active' => '1', 'entity_code' => '£', 'standard' => '1'];
         $currencies[] = ['name' => 'Hong Kong Dollar', 'iso' => 'HKD', 'numeric_code' => '344', 'active' => '0', 'entity_code' => 'HK$', 'standard' => '0'];
         $currencies[] = ['name' => 'Croatian Kuna', 'iso' => 'HRK', 'numeric_code' => '191', 'active' => '0', 'entity_code' => 'kuna', 'standard' => '0'];
-        $currencies[] = ['name' => 'Forint', 'iso' => 'HUF', 'numeric_code' => '348', 'active' => '0', 'entity_code' => 'Ft', 'standard' => '0'];
-        $currencies[] = ['name' => 'Rupiah', 'iso' => 'IDR', 'numeric_code' => '360', 'active' => '0', 'entity_code' => 'Rp', 'standard' => '0'];
-        $currencies[] = ['name' => 'New Israeli Sheqel', 'iso' => 'ILS', 'numeric_code' => '376', 'active' => '0', 'entity_code' => '₪', 'standard' => '0'];
+        $currencies[] = ['name' => 'Hungarian Forint', 'iso' => 'HUF', 'numeric_code' => '348', 'active' => '0', 'entity_code' => 'Ft', 'standard' => '0'];
+        $currencies[] = ['name' => 'Indonesian Rupiah', 'iso' => 'IDR', 'numeric_code' => '360', 'active' => '0', 'entity_code' => 'Rp', 'standard' => '0'];
+        $currencies[] = ['name' => 'Israeli Sheqel', 'iso' => 'ILS', 'numeric_code' => '376', 'active' => '0', 'entity_code' => '₪', 'standard' => '0'];
         $currencies[] = ['name' => 'Indian Rupee', 'iso' => 'INR', 'numeric_code' => '356', 'active' => '0', 'entity_code' => '₹', 'standard' => '0'];
-        $currencies[] = ['name' => 'Yen', 'iso' => 'JPY', 'numeric_code' => '392', 'active' => '0', 'entity_code' => '¥', 'standard' => '0'];
-        $currencies[] = ['name' => 'Won', 'iso' => 'KRW', 'numeric_code' => '410', 'active' => '0', 'entity_code' => '₩', 'standard' => '0'];
+        $currencies[] = ['name' => 'Icelandic Krona', 'iso' => 'ISK', 'numeric_code' => '352', 'active' => '0', 'entity_code' => 'kr', 'standard' => '0'];
+        $currencies[] = ['name' => 'Japenese Yen', 'iso' => 'JPY', 'numeric_code' => '392', 'active' => '0', 'entity_code' => '¥', 'standard' => '0'];
+        $currencies[] = ['name' => 'South Korean Won', 'iso' => 'KRW', 'numeric_code' => '410', 'active' => '0', 'entity_code' => '₩', 'standard' => '0'];
         $currencies[] = ['name' => 'Lithuanian Litas', 'iso' => 'LTL', 'numeric_code' => '440', 'active' => '0', 'entity_code' => 'Lt', 'standard' => '0'];
         $currencies[] = ['name' => 'Latvian Lats', 'iso' => 'LVL', 'numeric_code' => '428', 'active' => '0', 'entity_code' => 'Ls', 'standard' => '0'];
         $currencies[] = ['name' => 'Mexican Peso', 'iso' => 'MXN', 'numeric_code' => '484', 'active' => '0', 'entity_code' => 'Mex$', 'standard' => '0'];
@@ -54,15 +55,15 @@ class CurrencySeeder extends Seeder
         $currencies[] = ['name' => 'Norwegian Krone', 'iso' => 'NOK', 'numeric_code' => '578', 'active' => '0', 'entity_code' => 'kr', 'standard' => '0'];
         $currencies[] = ['name' => 'New Zealand Dollar', 'iso' => 'NZD', 'numeric_code' => '554', 'active' => '0', 'entity_code' => 'NZ$', 'standard' => '0'];
         $currencies[] = ['name' => 'Philippine Peso', 'iso' => 'PHP', 'numeric_code' => '608', 'active' => '0', 'entity_code' => '₱', 'standard' => '0'];
-        $currencies[] = ['name' => 'Zloty', 'iso' => 'PLN', 'numeric_code' => '985', 'active' => '0', 'entity_code' => 'zł', 'standard' => '0'];
-        $currencies[] = ['name' => 'Leu', 'iso' => 'RON', 'numeric_code' => '946', 'active' => '0', 'entity_code' => 'leu', 'standard' => '0'];
+        $currencies[] = ['name' => 'Polish Zloty', 'iso' => 'PLN', 'numeric_code' => '985', 'active' => '0', 'entity_code' => 'zł', 'standard' => '0'];
+        $currencies[] = ['name' => 'Romanian Leu', 'iso' => 'RON', 'numeric_code' => '946', 'active' => '0', 'entity_code' => 'leu', 'standard' => '0'];
         $currencies[] = ['name' => 'Russian Ruble', 'iso' => 'RUB', 'numeric_code' => '643', 'active' => '0', 'entity_code' => 'PP', 'standard' => '0'];
         $currencies[] = ['name' => 'Swedish Krona', 'iso' => 'SEK', 'numeric_code' => '752', 'active' => '0', 'entity_code' => 'kr', 'standard' => '0'];
         $currencies[] = ['name' => 'Singapore Dollar', 'iso' => 'SGD', 'numeric_code' => '702', 'active' => '0', 'entity_code' => 'S$', 'standard' => '0'];
-        $currencies[] = ['name' => 'Baht', 'iso' => 'THB', 'numeric_code' => '764', 'active' => '0', 'entity_code' => '฿', 'standard' => '0'];
+        $currencies[] = ['name' => 'Thai Baht', 'iso' => 'THB', 'numeric_code' => '764', 'active' => '0', 'entity_code' => '฿', 'standard' => '0'];
         $currencies[] = ['name' => 'Turkish Lira', 'iso' => 'TRY', 'numeric_code' => '949', 'active' => '0', 'entity_code' => 'TL', 'standard' => '0'];
         $currencies[] = ['name' => 'US Dollar', 'iso' => 'USD', 'numeric_code' => '840', 'active' => '1', 'entity_code' => '$', 'standard' => '0'];
-        $currencies[] = ['name' => 'Rand', 'iso' => 'ZAR', 'numeric_code' => '710', 'active' => '0', 'entity_code' => 'R', 'standard' => '0'];
+        $currencies[] = ['name' => 'South African Rand', 'iso' => 'ZAR', 'numeric_code' => '710', 'active' => '0', 'entity_code' => 'R', 'standard' => '0'];
 
         foreach ($currencies as $id => $currency) {
             $currencies[$id] = array_merge($currency, ['created_at' => $createdAt, 'updated_at' => $createdAt]);
