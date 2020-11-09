30811976538412603897eab67b7e6f9b639ddd08
Frank Pillukeit <clients@pw-websolutions.com>
2020-11-09 15:01:41 +0100

    Adds temporary profile views.

---
+++ src/resources/views/profile/header.blade.php
@@ -0,0 +1,123 @@
+<div class="bg-primary">
+    <div class="container pad-t-1">
+        <div class="row">
+            <div class="col-lg-5 order-lg-2 text-lg-right mb-4 mb-lg-0">
+                <div class="d-flex d-lg-inline-block justify-content-between justify-content-lg-end align-items-center align-items-lg-start">
+                    <!-- Breadcrumb -->
+                    <ol class="breadcrumb breadcrumb-white breadcrumb-no-gutter mb-0">
+                        <li class="breadcrumb-item"><a class="breadcrumb-link" href="{{ route('root')}}">{{ __('Home') }}</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">{{ __('Edit profile') }}</li>
+                    </ol>
+                    <!-- End Breadcrumb -->
+                    <!-- Breadcrumb Nav Toggle Button -->
+                    <div class="d-lg-none">
+                        <button type="button" class="navbar-toggler btn u-hamburger u-hamburger--white" aria-label="Toggle navigation" aria-expanded="false" aria-controls="breadcrumbNavBar" data-toggle="collapse" data-target="#breadcrumbNavBar">
+                            <span id="breadcrumbHamburgerTrigger" class="u-hamburger__box">
+                                <span class="u-hamburger__inner"></span>
+                            </span>
+                        </button>
+                    </div>
+                    <!-- End Breadcrumb Nav Toggle Button -->
+                </div>
+            </div>
+            <div class="col-lg-7 order-lg-1">
+                <!-- User Info -->
+                <div class="media d-block d-sm-flex align-items-sm-center">
+                    <div class="avatar-lg position-relative mb-3 mb-sm-0 mr-3">
+                        <img class="img-fluid rounded-circle" src="{{ Auth::user()->person->avatar }}" alt="{{ Auth::user()->person->display_name }}" />
+                        <span class="badge badge-md badge-outline-success badge-pos badge-pos-bottom-right rounded-circle">
+                            <span class="fas fa-check"></span>
+                        </span>
+                    </div>
+                    <div class="media-body">
+                        <h1 class="h3 text-white font-weight-medium mb-1">Howdy, {{ Auth::user()->person->name }}!</h1>
+                        <span class="d-block text-white">{{ Auth::user()->email }}</span>
+                    </div>
+                </div>
+                <!-- End User Info -->
+            </div>
+        </div>
+    </div>
+    <div class="container pad-b-1 space-bottom-lg-0">
+        <div class="d-lg-flex justify-content-lg-between align-items-lg-center">
+            <!-- Navbar -->
+            <div class="u-header u-header-left-aligned-nav u-header--bg-transparent-lg u-header--white-nav-links z-index-4">
+                <div class="u-header__section bg-transparent">
+                    <nav class="js-breadcrumb-menu navbar navbar-expand-lg u-header__navbar u-header__navbar--no-space hs-menu-initialized hs-menu-horizontal">
+                        <div id="breadcrumbNavBar" class="navbar-collapse u-header__navbar-collapse">
+                            <ul class="navbar-nav u-header__navbar-nav">
+                                <li class="nav-item hs-has-sub-menu u-header__nav-item" data-event="hover" data-animation-in="slideInUp" data-animation-out="fadeOut">
+                                    <a id="generalDropdown" class="nav-link u-header__nav-link u-header__nav-link-toggle" href="#" aria-haspopup="true" aria-expanded="false" aria-labelledby="generalDropdownMenu">
+                                        General
+                                    </a>
+
+                  <ul id="generalDropdownMenu" class="hs-sub-menu u-header__sub-menu u-header__sub-menu--spacer" style="min-width: 230px; display: none;" aria-labelledby="generalDropdown">
+                    <li><a class="nav-link" href="/home">Dashboard</a></li>
+                    <li><a class="nav-link" href="profile.html">Profile</a></li>
+                    <li><a class="nav-link" href="my-tasks.html">My tasks</a></li>
+                    <li><a class="nav-link" href="projects.html">Projects</a></li>
+                    <li><a class="nav-link" href="members.html">Members</a></li>
+                  </ul>
+                </li>
+                <!-- General -->
+
+                <!-- Account Settings -->
+                <li class="nav-item hs-has-sub-menu u-header__nav-item" data-event="hover" data-animation-in="slideInUp" data-animation-out="fadeOut">
+                  <a id="accountSettingsDropdown" class="nav-link u-header__nav-link u-header__nav-link-toggle" href="javascript:;" aria-haspopup="true" aria-expanded="false" aria-labelledby="accountSettingsDropdownMenu">
+                    Account settings
+                  </a>
+
+                  <ul id="accountSettingsDropdownMenu" class="hs-sub-menu u-header__sub-menu u-header__sub-menu--spacer" style="min-width: 230px; display: none;" aria-labelledby="accountSettingsDropdown">
+                    <li><a class="nav-link u-header__sub-menu-nav-link" href="edit-profile.html">Edit profile</a></li>
+                    <li><a class="nav-link u-header__sub-menu-nav-link" href="change-password.html">Change password</a></li>
+                    <li><a class="nav-link u-header__sub-menu-nav-link" href="notifications.html">Notifications</a></li>
+                  </ul>
+                </li>
+                <!-- Account Settings -->
+
+                <!-- Billing -->
+                <li class="nav-item hs-has-sub-menu u-header__nav-item" data-event="hover" data-animation-in="slideInUp" data-animation-out="fadeOut">
+                  <a id="billingDropdown" class="nav-link u-header__nav-link u-header__nav-link-toggle" href="javascript:;" aria-haspopup="true" aria-expanded="false" aria-labelledby="billingDropdownMenu">
+                    Billing
+                  </a>
+
+                  <ul id="billingDropdownMenu" class="hs-sub-menu u-header__sub-menu u-header__sub-menu--spacer animated fadeOut" style="min-width: 230px; display: none;" aria-labelledby="billingDropdown">
+                    <li><a class="nav-link u-header__sub-menu-nav-link" href="activity.html">Activity</a></li>
+                    <li><a class="nav-link u-header__sub-menu-nav-link" href="payment-methods.html">Payment methods</a></li>
+                  </ul>
+                </li>
+                <!-- Billing -->
+
+                <!-- Accessibility -->
+                <li class="nav-item hs-has-sub-menu u-header__nav-item" data-event="hover" data-animation-in="slideInUp" data-animation-out="fadeOut">
+                  <a id="accessibilityDropdown" class="nav-link u-header__nav-link u-header__nav-link-toggle" href="javascript:;" aria-haspopup="true" aria-expanded="false" aria-labelledby="accessibilityDropdownMenu">
+                    Accessibility
+                  </a>
+
+                  <ul id="accessibilityDropdownMenu" class="hs-sub-menu u-header__sub-menu u-header__sub-menu--spacer animated fadeOut" style="min-width: 230px; display: none;" aria-labelledby="accessibilityDropdown">
+                    <li><a class="nav-link u-header__sub-menu-nav-link" href="invite-friends.html">Invite friends</a></li>
+                    <li><a class="nav-link u-header__sub-menu-nav-link" href="api-token.html">API Token</a></li>
+                  </ul>
+                </li>
+                <!-- Accessibility -->
+
+                <!-- Others -->
+                <li class="nav-item u-header__nav-item">
+                  <a class="nav-link u-header__nav-link" href="plans.html">Plans</a>
+                </li>
+                <!-- Others -->
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+      <!-- End Navbar -->
+
+      <div class="ml-lg-auto">
+        <!-- Button -->
+        <a class="btn btn-sm btn-soft-white transition-3d-hover" href="{{ route('system.profile.index') }}">{{ __('View My Profile') }}</a>
+        <!-- End Button -->
+      </div>
+    </div>
+    </div>
+</div>
---
+++ src/resources/views/profile/public.blade.php
@@ -0,0 +1,135 @@
+@extends('layouts.app')
+
+@section('content')
+<main id="content" role="main">
+    <!-- Breadcrumb Section -->
+    <div class="bg-primary">
+        <div class="container pad-1">
+            <div class="d-sm-flex justify-content-sm-between align-items-sm-center">
+                <div class="mb-3 mb-sm-0">
+                    <!-- Breadcrumb -->
+                    <ol class="breadcrumb breadcrumb-white breadcrumb-no-gutter mb-0">
+                        <li class="breadcrumb-item"><a class="breadcrumb-link" href="{{ route('root') }}">{{ __('Home') }}</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">{{ __('Profile') }}</li>
+                    </ol>
+                    <!-- End Breadcrumb -->
+                </div>
+                <!-- Edit Profile -->
+                <a class="btn btn-sm btn-soft-white transition-3d-hover" href="{{ route('system.profile.befriend', $profile->username) }}">
+                    <span class="fas fa-user-plus small mr-2"></span>
+                    {{ __('Send friend request') }}
+                </a>
+                <!-- End Edit Profile -->
+            </div>
+        </div>
+    </div>
+    <!-- End Breadcrumb Section -->
+
+    <!-- Content Section -->
+    <div class="bg-light">
+        <div class="container pad-1">
+            <div class="row">
+                <div class="col">
+                    @include('flash::message')
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 mb-7 mb-lg-0">
+                    <!-- Profile Card -->
+                    <div class="card p-1 mb-4">
+                        <div class="card-body text-center">
+                            <div class="mb-3">
+                                <img class="avatar-lg rounded-circle" src="{{ $profile->person->avatar }}" alt="{{ $profile->person->display_name }}" />
+                            </div>
+                            <div class="mb-3">
+                                <h1 class="h6 font-weight-medium mb-0">{{ $profile->person->title }} {{ $profile->person->display_name }}</h1>
+                                <small class="d-block text-muted">{{ $profile->person->description}}</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mb-4">
+                        <div class="card-header pt-4 pb-3 px-0 mx-4">
+                            <h2 class="h6 mb-0">Contacts</h2>
+                        </div>
+                        <div class="card-body pt-3 pb-4 px-4">
+                            @foreach($profile->getFriends() as $friend)
+                            <!-- User -->
+                            <a class="d-flex align-items-start mb-4" href="#">
+                                <div class="position-relative u-avatar">
+                                    <img class="img-fluid rounded-circle" src="{{ $friend->person->avatar }}" alt="{{ $friend->person->display_name }}">
+                                    <span class="badge badge-xs badge-outline-success badge-pos badge-pos--bottom-right rounded-circle"></span>
+                                </div>
+
+                                <div class="ml-3">
+                                    <span class="d-block text-dark">{{ $friend->person->display_name }}</span>
+                                    {{-- <small class="d-block text-secondary">Web Developer</small> --}}
+                                </div>
+                            </a>
+                            <!-- End User -->
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-9">
+                    <div class="mb-4">
+                        <h2 class="h4">Hey, I am {{ $profile->person->display_name }} <span class="badge badge-secondary ml-1">Pro</span></h2>
+                        <h4 class="h6 text-secondary mb-0">
+                            @isnotnull($profile->person->home)
+                            {{ $profile->person->home->city }}, {{ $profile->person->home->country->iso }} <small>&dash;
+                            @else
+                                <small>
+                            @endisnotnull
+                            Joined in {{ $profile->joined->format('M Y') }}</small></h4>
+                    </div>
+                    @if($profile->hasVerifiedEmail() === false)
+                    <div class="alert alert-danger">You have not verified your email. If you have not received an email, click <a href="{{ route('system.profile.reverify') }}">here</a> to resend it.</div>
+                    @endif
+                    <div class="mb-4">
+                        <p>DESCRIPTION</p>
+                    </div>
+                    <hr class="my-7">
+                    <div class="row">
+                        <div class="col-md-6 col-lg-4">
+                            <ul class="list-group list-group-transparent list-group-flush list-group-borderless mb-0">
+                                <li class="list-group-item pt-0 pb-4">
+                                    <div class="media">
+                                        <span class="fas fa-map-marker-alt list-group-icon mr-3"></span>
+                                        <div class="media-body text-lh-sm">
+                                            <span class="d-block mb-1">Location:</span>
+                                            @isnotnull($profile->person->home)
+                                            <a href="#">{{ $profile->person->home->city }}, {{ $profile->person->home->country->name }}</a>
+                                            @else
+                                                &dash;
+                                            @endisnotnull
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-6 col-lg-4">
+                            <ul class="list-group list-group-transparent list-group-flush list-group-borderless mb-0">
+                                <li class="list-group-item pt-0 pb-4">
+                                    <div class="media">
+                                        <span class="fas fa-birthday-cake list-group-icon mr-3"></span>
+                                        <div class="media-body text-lh-sm">
+                                            <span class="d-block mb-1">Birthday:</span>
+                                            <span class="d-block text-muted">
+                                                @date($profile->person->dob, 'd. M Y')
+                                            </span>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-md-6 col-lg-4">
+                            <ul class="list-group list-group-transparent list-group-flush list-group-borderless mb-0">
+                            </ul>
+                        </div>
+                    </div>
+                    <hr class="my-7">
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+@endsection
