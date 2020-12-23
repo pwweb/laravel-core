400a27553e2de3808ecae5e52e3948a2cf6d3513
Frank Pillukeit <clients@pw-websolutions.com>
2020-12-23 15:57:26 +0100

    ✳️ Adds title enum test

---
+++ tests/Unit/Enums/TitleTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace PWWEB\Core\Tests\Unit\Enums;
+
+use BadMethodCallException;
+use PHPUnit\Framework\TestCase;
+use PWWEB\Core\Enums\Title;
+use TypeError;
+
+class TitleTest extends TestCase
+{
+    /** @test */
+    public function enums_can_be_constructed()
+    {
+        'none' => 0,
+        'dr' => 1,
+        'prof' => 2,
+        'profdr' => 3,
+        'eng' => 4,
+        'dipling' => 5,
+
+        $noTitle = Title::none();
+        $drTitle = Title::dr();
+        $profTitle = Title::prof();
+        $profDrTitle = Title::profdr();
+        $engTitle = Title::eng();
+        $diplIngTitle = Title::dipling();
+
+        $this->assertInstanceOf(Title::class, $noTitle);
+        $this->assertInstanceOf(Title::class, $drTitle);
+        $this->assertInstanceOf(Title::class, $profTitle);
+        $this->assertInstanceOf(Title::class, $profDrTitle);
+        $this->assertInstanceOf(Title::class, $engTitle);
+        $this->assertInstanceOf(Title::class, $diplIngTitle);
+    }
+
+    /** @test */
+    public function unknown_enum_method_triggers_exception()
+    {
+        $this->expectException(BadMethodCallException::class);
+
+        Title::meng();
+    }
+
+    /** @test */
+    public function invalid_value_type_throws_exception()
+    {
+        $this->expectException(TypeError::class);
+
+        Title::make([]);
+    }
+
+    /** @test */
+    public function to_json()
+    {
+        $json = json_encode(Title::eng());
+
+        $this->assertEquals('"Eng."', $json);
+    }
+
+    /** @test */
+    public function to_string()
+    {
+        $drString = (string) Title::dr();
+
+        $this->assertEquals('Dr.', $maleString);
+    }
+}
