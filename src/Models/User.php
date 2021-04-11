ac76308b8e9b7f3420ee7707d95528dec3a6e2b7
Richard Browne <r.a.browne@gmail.com>
2021-04-11 13:26:55 +0100

    👮‍♂️ Security fix to hide password from toArray/toJson commands.

--- src/Models/User.php
+++ src/Models/User.php
@@ -53,6 +53,7 @@ class User extends Authenticatable implements MustVerifyEmail, HasLocalePreferen
      */
     protected $hidden = [
         'remember_token',
+        'password',
     ];
 
     /**
